2b0d07d0f90ee6bb6ee82d2975feb460c8565a6f
Gabriel <oliveirafrerreira97@hotmail.com>
2025-01-26 22:12:12 -0300

    implementação dos métodos restantes do crud e criação do método abstrato findByNome no ProdutoRepository

--- src/main/java/github/com/gabrielbkx/produtosapi/controller/ProdutoController.java
+++ src/main/java/github/com/gabrielbkx/produtosapi/controller/ProdutoController.java
@@ -3,11 +3,9 @@ package github.com.gabrielbkx.produtosapi.controller;
 
 import github.com.gabrielbkx.produtosapi.model.Produto;
 import github.com.gabrielbkx.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -30,6 +28,29 @@ public class ProdutoController{
 
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
+
+      return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void excluirPorId(@PathVariable("id") String id){
+          produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id,
+                          @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
     }
 
 }
